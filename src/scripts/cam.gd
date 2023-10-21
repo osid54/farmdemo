e7bb9e605b4f1df74d3ff3d0a8c587509d46d0d0
osid54 <osid.potato@gmail.com>
2023-10-20 22:39:35 -0400

    better camera

--- src/scripts/cam.gd
+++ src/scripts/cam.gd
@@ -1,6 +1,7 @@
 extends Camera2D
 
 @export var speed := 0
+var fixed_toggle_point = Vector2(0,0)
 
 func _process(delta):
 	var direction = Vector2.ZERO
@@ -16,5 +17,22 @@ func _process(delta):
 	
 	position += direction * speed * delta
 
+	if Input.is_action_just_pressed("moveMap"):
+		var ref = get_viewport().get_mouse_position()
+		fixed_toggle_point = ref
+	if Input.is_action_pressed("moveMap"):
+		slide_map_around()
+	if Input.is_action_just_released("zoomMapIn"):
+		zoom += Vector2(.1,.1)
+	if Input.is_action_just_released("zoomMapOut"):
+		zoom -= Vector2(.1,.1)
+	
 	position.x = clamp(position.x,-1000,1000)
 	position.y = clamp(position.y,-1000,1000)
+	zoom = zoom.clamp(Vector2(.5,.5),Vector2(1.5,1.5))
+
+# slides the map around
+func slide_map_around():
+	var ref = get_viewport().get_mouse_position()
+	self.global_position.x += (ref.x - fixed_toggle_point.x) / 80
+	self.global_position.y += (ref.y - fixed_toggle_point.y) / 80
